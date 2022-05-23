37d52a7e37e1c8770c6e21be161c68fd99a32219
Neo-Info <21brgames@gmail.com>
2022-05-23 13:31:03 -0300

    exeRestApi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React from 'react'
+import { useState } from 'react';
+import {set, useForm} from 'react-hook-form'
+
 import './App.css';
 
 function App() {
+
+  const [cep, setCep] = useState('')
+  const [end, setEnd] = useState('')
+  const [comp, setComp] = useState('')
+  const [bair, setBair] = useState('')
+  const [cid, setCid] = useState('')
+  const [est, setEst] = useState('')
+  const [ddd, setDDD] = useState('')
+
+  const {register, handleSubmit} = useForm()
+  
+  const buscaCep = async (e) => {
+    const res = await fetch('https://viacep.com.br/ws/'+ e.cep +'/json/')
+    /* aguarda a reposta e trasforma em json */
+    .then((res) => res.json())
+    
+    console.log(res)
+    setCep(res.cep)
+    setEnd(res.logradouro)
+    setComp(res.complemento)
+    setBair(res.bairro)
+    setCid(res.localidade)
+    setEst(res.uf)
+    setDDD(res.ddd)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <h2>React BuscaCep</h2>
+      </div>
+      <div className='form'>
+        <form onSubmit={handleSubmit(buscaCep)}>
+          <div className='form-control'>
+            <label htmlFor='cep'>Numero CEP</label>
+            <input type='text' {...register('cep')} placeholder='insira o cep'></input>
+          </div>
+          <div className='form-control'>
+            <button type='submit'>Pesquisar</button>
+          </div>
+        </form>
+      </div>
+      <div className='result'>
+        <p>Cep: {cep}</p>
+        <p>Endereço: {end}</p>
+        <p>Complemento: {comp}</p>
+        <p>Bairro: {bair}</p>
+        <p>Cidade: {cid}</p>
+        <p>Estado: {est}</p>
+        <p>DDD: {ddd}</p>
+      </div>
     </div>
   );
 }
